9e0bcdf60494b655b9375e1f6004c6c8a43e5256
2177meetpatel <patelmeet21771906@gmail.com>
2026-05-27 15:17:29 +0530

    Added dynamic product grid

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,3 +6,48 @@ const navLinks = document.querySelector(".nav-links");
 hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
+/* FETCH PRODUCTS */
+
+const productGrid = document.querySelector(".product-grid");
+
+/* API CALL */
+
+fetch("https://fakestoreapi.com/products")
+    .then(response => response.json())
+    .then(data => {
+
+        data.forEach(product => {
+
+            productGrid.innerHTML += `
+
+                <div class="product-card">
+
+                    <img src="${product.image}" 
+                    alt="${product.title}"
+                    loading="lazy">
+
+                    <div class="product-info">
+
+                        <h3 class="product-title">
+                            ${product.title}
+                        </h3>
+
+                        <p class="product-price">
+                            $${product.price}
+                        </p>
+
+                        <button class="product-btn">
+                            Add to Cart
+                        </button>
+
+                    </div>
+
+                </div>
+
+            `;
+        });
+
+    })
+    .catch(error => {
+        console.log("Error fetching products:", error);
+    });
